3df9a7b7e05ad99a54cf3f99301d35ecdb332244
sunnymehta19 <whosunnymehta@gmail.com>
2026-02-15 19:03:40 +0530

    fix frontend case sensitivity issue

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 import { Provider } from "react-redux";
-import store from './Store/Store.js';
+import store from './Store/store.js';
 import { Flip, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
