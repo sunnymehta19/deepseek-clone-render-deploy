8dbb1f5edfa1f2c0d348bc4985338235686262e5
sunnymehta19 <whosunnymehta@gmail.com>
2026-02-15 22:51:29 +0530

    change loading screen

--- frontend/src/Components/AppSkeleton.jsx
+++ frontend/src/Components/AppSkeleton.jsx
@@ -4,6 +4,7 @@ const AppSkeleton = () => {
   return (
     <div className="flex h-screen bg-[#151517] text-white animate-pulse">
 
+      {/* Sidebar */}
       <div className="hidden md:flex flex-col w-66 bg-[#1b1b1c] p-4 space-y-6">
         <div className="h-6 w-28 bg-[#2c2c2e] rounded-md" />
         <div className="h-10 w-full bg-[#2c2c2e] rounded-full" />
@@ -18,23 +19,28 @@ const AppSkeleton = () => {
         <div className="mt-auto h-10 w-full bg-[#2c2c2e] rounded-xl" />
       </div>
 
-      <div className="flex-1 flex flex-col px-4 md:px-8 py-6 space-y-8">
+      {/* Main Chat Area */}
+      <div className="flex-1 flex justify-center py-6">
+        <div className="w-full max-w-4xl px-4 md:px-8 flex flex-col space-y-8">
 
-        <div className="flex justify-end">
-          <div className="h-10 w-60 bg-[#2c2c2e] rounded-3xl" />
-        </div>
+          <div className="flex justify-end">
+            <div className="h-10 w-60 bg-[#2c2c2e] rounded-3xl" />
+          </div>
 
-        <div className="space-y-4 max-w-3xl">
-          <div className="h-4 w-40 bg-[#2c2c2e] rounded-md" />
-          <div className="h-4 w-full bg-[#2c2c2e] rounded-md" />
-          <div className="h-4 w-5/6 bg-[#2c2c2e] rounded-md" />
-          <div className="h-4 w-4/6 bg-[#2c2c2e] rounded-md" />
-        </div>
+          <div className="space-y-4">
+            <div className="h-4 w-40 bg-[#2c2c2e] rounded-md" />
+            <div className="h-4 w-full bg-[#2c2c2e] rounded-md" />
+            <div className="h-4 w-5/6 bg-[#2c2c2e] rounded-md" />
+            <div className="h-4 w-4/6 bg-[#2c2c2e] rounded-md" />
+          </div>
 
-        <div className="mt-auto max-w-3xl">
-          <div className="h-20 w-full bg-[#2c2c2e] rounded-3xl" />
+          <div className="mt-auto">
+            <div className="h-20 w-full bg-[#2c2c2e] rounded-3xl" />
+          </div>
+
         </div>
       </div>
+
     </div>
   );
 };
